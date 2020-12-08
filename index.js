19dc07f55b9565fce8cdafed94968915590baad1
Grayson Hicks <graysonhicks@gmail.com>
2020-12-08 16:02:37 -0500

    misnamed file fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('./jest-common'),
+  ...require('./jest.common'),
   collectCoverageFrom: ['**/src/**/*.js', '!**/src/**/*.stories.js'],
   coveragePathIgnorePatterns: [
     '/node_modules/',
